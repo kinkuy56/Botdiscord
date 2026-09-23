2e358ad03c5c4c89e507bf17c37396ab76e0de25
kinkuy56 <kirakorn70@gmail.com>
2026-09-23 14:00:56 +0700

    Update waguribot.py

--- waguribot.py
+++ waguribot.py
@@ -1,3 +1,24 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# เรียกใช้งานฟังก์ชันเปิดเว็บจำลอง
+keep_alive()
+
 import nextcord
 import json 
 from nextcord.ext import commands, tasks
